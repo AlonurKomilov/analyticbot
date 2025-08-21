4b3b2f117f6c0992da8e0f95ad63f006a904edea
AlonurKomilov <abclegacyllc@gmail.com>
2025-08-21 18:09:40 +0000

    08/21/2025

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -4,4 +4,4 @@ from bot.locales.i18n_hub import I18N_HUB
 
 # I18nMiddleware obyektini yaratish va eksport qilish
 # Tilni boshqarish logikasi run_bot.py ichidagi LanguageManager'da amalga oshiriladi
-i18n_middleware = I18nMiddleware(i18n=I18N_HUB, default_locale="en")
+i18n_middleware = I18nMiddleware(I18N_HUB, default_locale="en")
